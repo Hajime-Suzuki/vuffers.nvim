841f6ce9d5b512e1b5e3133cf80916952af57de1
Hajime Suzuki <hajime.doublebass@gmail.com>
2023-04-25 07:22:33 +0200

    fix: keymaps

--- lua/vuffers/key-bindings.lua
+++ lua/vuffers/key-bindings.lua
@@ -1,13 +1,32 @@
 local logger = require("utils.logger")
 local actions = require("vuffers.buffer-actions")
+local config = require("vuffers.config")
 
 local M = {}
 
 ---@param bufnr integer
 function M.setup(bufnr)
-  logger.debug("set key bindings")
-  vim.keymap.set("n", "<CR>", actions.open_buffer, { noremap = true, silent = true, nowait = true, buffer = bufnr })
-  vim.keymap.set("n", "d", actions.delete_buffer, { noremap = true, silent = true, nowait = true, buffer = bufnr })
+  local keymaps = config.get_keymaps()
+  if not keymaps.use_default then
+    logger.debug("skipping setting key bindings")
+    return
+  end
+
+  logger.debug("setting key bindings", keymaps)
+
+  vim.keymap.set(
+    "n",
+    keymaps.view.open,
+    actions.open_buffer,
+    { noremap = true, silent = true, nowait = true, buffer = bufnr }
+  )
+
+  vim.keymap.set(
+    "n",
+    keymaps.view.delete,
+    actions.delete_buffer,
+    { noremap = true, silent = true, nowait = true, buffer = bufnr }
+  )
 end
 
 return M
