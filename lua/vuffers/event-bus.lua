77a23fd9e69589e57e91d3000b922d2c1e99ce3b
Hajime Suzuki <hajime.doublebass@gmail.com>
2023-04-26 08:05:47 +0200

    add publish method

--- lua/vuffers/event-bus.lua
+++ lua/vuffers/event-bus.lua
@@ -1,13 +1,14 @@
 local logger = require("utils.logger")
+local list = require("utils.list")
 
 local M = {}
 
----@type table<string, {handler: function, label?: string}>
+---@type table<string, {handler: function, label: string}[]>
 local _subscribers = {}
 
 ---@param event Event
 ---@param handler function
----@param opts? {label: string}
+---@param opts {label: string}
 function M.subscribe(event, handler, opts)
   if _subscribers[event] then
     _subscribers[event] = {}
@@ -15,13 +16,25 @@ function M.subscribe(event, handler, opts)
 
   table.insert(_subscribers[event], handler)
 
-  local label = (opts and opts.label and opts.label or "")
-  logger.debug("subscribed to event: " .. event .. (label and " (" .. label .. ")" or ""))
+  local label = opts.label
+  logger.debug("subscribed to event: " .. event .. " (" .. label .. ")")
 end
 
 ---@param event Event
-function M.publish(event)
-  print("published!")
+---@param payload? any
+function M.publish(event, payload)
+  logger.debug("receiving event: " .. event, { event = event, payload = payload })
+
+  local handlers = _subscribers[event]
+  if not #handlers then
+    logger.debug("no handler found", { event = event })
+    return
+  end
+
+  list.for_each(handlers, function(handler)
+    handler.handler(payload)
+    logger.debug("publish event: " .. event, { target = handler.label, payload = payload })
+  end)
 end
 
 return M
