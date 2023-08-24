87796af0be1dfe5e3674f13e89a41f29af740384
Hajime Suzuki <hajime.doublebass@gmail.com>
2023-08-24 09:00:39 +0200

    add rename keymaps to config

--- lua/vuffers/key-bindings.lua
+++ lua/vuffers/key-bindings.lua
@@ -1,7 +1,6 @@
 local logger = require("utils.logger")
 local actions = require("vuffers.ui-actions")
 local config = require("vuffers.config")
-local buffers = require("vuffers.buffers")
 
 local M = {}
 
@@ -16,46 +15,17 @@ function M.setup(payload)
 
   logger.debug("setting key bindings", keymaps)
 
-  vim.keymap.set(
-    "n",
-    keymaps.view.open,
-    actions.open_buffer,
-    { noremap = true, silent = true, nowait = true, buffer = bufnr }
-  )
+  local opts = { noremap = true, silent = true, nowait = true, buffer = bufnr }
 
-  vim.keymap.set(
-    "n",
-    keymaps.view.delete,
-    actions.delete_buffer,
-    { noremap = true, silent = true, nowait = true, buffer = bufnr }
-  )
+  vim.keymap.set("n", keymaps.view.open, actions.open_buffer, opts)
 
-  vim.keymap.set(
-    "n",
-    keymaps.view.pin,
-    actions.pin_buffer,
-    { noremap = true, silent = true, nowait = true, buffer = bufnr }
-  )
+  vim.keymap.set("n", keymaps.view.delete, actions.delete_buffer, opts)
 
-  vim.keymap.set(
-    "n",
-    keymaps.view.unpin,
-    actions.unpin_buffer,
-    { noremap = true, silent = true, nowait = true, buffer = bufnr }
-  )
+  vim.keymap.set("n", keymaps.view.pin, actions.pin_buffer, opts)
 
-  vim.keymap.set("n", "R", function()
-    local pos = vim.api.nvim_win_get_cursor(0)
-    local b = buffers.get_buffer_by_index(pos[1])
-    vim.ui.input({ prompt = "new name? ", default = b.name }, function(new_name)
-      vim.notify(new_name)
-      if not new_name then
-        return
-      end
+  vim.keymap.set("n", keymaps.view.unpin, actions.unpin_buffer, opts)
 
-      buffers.rename_buffer({ index = pos[1], new_name = new_name })
-    end)
-  end, { noremap = true, silent = true, nowait = true, buffer = bufnr })
+  vim.keymap.set("n", keymaps.view.rename, actions.rename_buffer, opts)
 end
 
 return M
